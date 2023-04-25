ee32ee7dd61138dae9642dc1b4e2e75782508b9c
sadakatsrabon <md.hossain.sadakat@gmail.com>
2023-04-25 16:29:37 +0600

    static page updated

--- src/component/Statics/Statics.jsx
+++ src/component/Statics/Statics.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
+import { PieChart, Pie, Tooltip } from 'recharts';
 
-const Statics = () => {
-    return (
-        <div>
-            <div>tHIS IS STATICS PAGE</div>
-        </div>
-    );
+const AssignmentResultsPieChart = () => {
+  const data = [
+    { name: 'Assignment 1', value: 29 },
+    { name: 'Assignment 2', value: 48 },
+    { name: 'Assignment 3', value: 28 },
+    { name: 'Assignment 4', value: 30 },
+    { name: 'Assignment 5', value: 60 },
+    { name: 'Assignment 6', value: 58 },
+    { name: 'Assignment 7', value: 28 },
+  ];
+
+  return (
+    <div>
+        <h2 className='text-4xl text-center font-bold mt-10'>Here I want to show  my Assignment marks  in Programing hero.  <br />
+        -Sadakat Hossain Srabon</h2>
+        <PieChart width={800} height={400}>
+      <Pie
+        data={data}
+        dataKey="value"
+        nameKey="name"
+        cx="50%"
+        cy="50%"
+        outerRadius={80}
+        fill="#8884d8"
+        label
+      />
+      <Tooltip />
+    </PieChart>
+    </div>
+  );
 };
 
-export default Statics;
+export default AssignmentResultsPieChart;
